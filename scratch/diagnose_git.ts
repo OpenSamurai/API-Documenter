fd76ac99f8c6d80730979ba272a32bbbac757a3e
PraneethKulukuri26 <praneethkulukuri@gmail.com>
2026-06-17 19:21:11 +0530

    made

--- scratch/diagnose_git.ts
+++
@@ -1,9 +0,0 @@
-import { simpleGit } from 'simple-git';
-
-async function test() {
-    const git = simpleGit();
-    const status = await git.status();
-    console.log(JSON.stringify(status, null, 2));
-}
-
-test();
